2824244372c7df7976aed49709e1be65e9544109
Franck <fdesjeunes@gmail.com>
2023-12-15 21:24:43 +0100

    Adding locking screen

---
+++ bin/lock.sh
@@ -0,0 +1,9 @@
+i3lock -u \
+    -i ~/Images/clouded_sky.jpg \
+    -F \
+    -k \
+    --time-color="f4dbd6" \
+    --date-color="f4dbd6" \
+    --time-font="FiraCode Nerd Font" \
+    --date-font="FiraCode Nerd Font" \
+    --time-str="󰍁 %H:%M"
